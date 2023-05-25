2a4780e25da7511dcd67e4a144abc34736d229e0
1Eva1987 <115692772+1Eva1987@users.noreply.github.com>
2023-05-25 10:08:10 +0100

    ejs templates created a bit of css applied routing and navigation created, when user navigates manualy to /compose url he can add a post that appears in home page , each post can be accessed using url /posts/<post-title> and appears in a separate page

--- app.js
+++ app.js
@@ -1,14 +1,60 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const ejs = require("ejs");
+const _ = require("lodash");
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.set("viw engine", "ejs");
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+const homeContent =
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos amet deserunt repellendus illo nostrum aliquam laboriosam reiciendis illum fuga quos ea enim, et doloribus doloremque officia labore voluptate quaerat excepturi!";
+const aboutContent =
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos amet deserunt repellendus illo nostrum aliquam laboriosam reiciendis illum fuga quos ea enim, et doloribus doloremque officia labore voluptate quaerat excepturi!";
+const contactContent =
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos amet deserunt repellendus illo nostrum aliquam laboriosam reiciendis illum fuga quos ea enim, et doloribus doloremque officia labore voluptate quaerat excepturi!";
+const posts = [];
 
 app.get("/", (req, res) => {
-  res.send("hi");
+  res.render("home", { homeText: homeContent, postsArray: posts });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { aboutText: aboutContent });
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact", { contactText: contactContent });
+});
+
+app.get("/compose", (req, res) => {
+  res.render("compose");
+});
+
+app.post("/compose", (req, res) => {
+  const post = {
+    postTitle: req.body.title,
+    postText: req.body.textarea,
+  };
+  posts.push(post);
+  res.redirect("/");
+});
+
+app.get("/posts/:post", (req, res) => {
+  for (let i = 0; i < posts.length; i++) {
+    if (
+      _.replace(posts[i].postTitle, " ", "-").toLocaleLowerCase() ===
+      req.params.post
+    ) {
+      res.render("post", {
+        postTitle: posts[i].postTitle,
+        postText: posts[i].postText,
+      });
+      console.log("match found");
+    } else {
+      console.log("no");
+    }
+  }
 });
 
 app.listen(3000, () => {
