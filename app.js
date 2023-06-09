6fcd0a77cdd038d9d61e1f91b92a60cdb8564216
1Eva1987 <115692772+1Eva1987@users.noreply.github.com>
2023-06-09 13:23:19 +0100

    app connected to mongo db all previous functionality kept the same just using database instead

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const _ = require("lodash");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 const app = express();
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+// connecting to mongoDB
+mongoose
+  .connect(process.env.MONGODB_CON, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("conected to mongoDB"))
+  .catch((err) => "there is a problem conecting to mongoDB: " + err);
+
+postSchema = {
+  name: String,
+  text: String,
+};
+const Post = mongoose.model("Post", postSchema);
 
 const homeContent =
   "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos amet deserunt repellendus illo nostrum aliquam laboriosam reiciendis illum fuga quos ea enim, et doloribus doloremque officia labore voluptate quaerat excepturi!";
@@ -14,10 +31,17 @@ const aboutContent =
   "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos amet deserunt repellendus illo nostrum aliquam laboriosam reiciendis illum fuga quos ea enim, et doloribus doloremque officia labore voluptate quaerat excepturi!";
 const contactContent =
   "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos amet deserunt repellendus illo nostrum aliquam laboriosam reiciendis illum fuga quos ea enim, et doloribus doloremque officia labore voluptate quaerat excepturi!";
-const posts = [];
 
 app.get("/", (req, res) => {
-  res.render("home", { homeText: homeContent, postsArray: posts, lodash: _ });
+  Post.find({})
+    .then((posts) => {
+      res.render("home", {
+        homeText: homeContent,
+        postsArray: posts,
+        lodash: _,
+      });
+    })
+    .catch((err) => console.log(err));
 });
 
 app.get("/about", (req, res) => {
@@ -33,26 +57,30 @@ app.get("/compose", (req, res) => {
 });
 
 app.post("/compose", (req, res) => {
-  const post = {
-    postTitle: req.body.title,
-    postText: req.body.textarea,
-  };
-  posts.push(post);
-  res.redirect("/");
+  const postTitle = req.body.title;
+  const postText = req.body.textarea;
+  const post = new Post({ name: postTitle, text: postText });
+  post
+    .save()
+    .then(() => res.redirect("/"))
+    .catch((err) => console.log(err));
 });
 
 app.get("/posts/:post", (req, res) => {
-  for (let i = 0; i < posts.length; i++) {
-    if (
-      _.replace(posts[i].postTitle, " ", "-").toLocaleLowerCase() ===
-      req.params.post
-    ) {
-      res.render("post", {
-        postTitle: posts[i].postTitle,
-        postText: posts[i].postText,
-      });
+  console.log(req.params.post);
+  Post.find({}).then((posts) => {
+    for (let i = 0; i < posts.length; i++) {
+      if (
+        _.replace(posts[i].name, " ", "-").toLocaleLowerCase() ===
+        req.params.post
+      ) {
+        res.render("post", {
+          postTitle: posts[i].name,
+          postText: posts[i].text,
+        });
+      }
     }
-  }
+  });
 });
 
 app.listen(3000, () => {
